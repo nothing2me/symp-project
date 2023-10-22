86e0d73c2b4ba9b7110daa502f7304e6978742be
nothing2me <93677972+nothing2me@users.noreply.github.com>
2023-10-21 22:00:19 -0500

    Update draft.cpp
    added stuff, comments

--- draft.cpp
+++ draft.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main()
 {
-    char playerDecision, ch, characterClass;
+    char continueGame, characterClass;
+    const int NO_STAMINA = 0;
     int characterStamina = 25;
     int hungerBar = 0;
-    bool endgame = false;
+    string characterClassroom, playerDecision, locationBank;
 
-    // Intro prompt
-    cout << "You just finished playing a game of chess in the library and feel quite hungry, your next class begins in an hour and 30 minutes." << endl;
-    cout << "Make it to the dining hall to quench your hunger before you make it to your next class." << endl << endl;
-    
+
     // Class pick menu
     cout << "Please pick your class type:" << endl;
     cout << "Athlete Class (a) - Starts with protein bars" << "           " << "Geek Class (g) - Starts with umbrella" << endl;
@@ -21,9 +19,11 @@ int main()
     // Cases for each class, needs to include items into player inventory before break also
     switch (characterClass)
     {
-        case 'a': cout << "The Athlete starts with 1 special protein bar (More energy = more moves)" << endl;
+        case 'a': cout << "The Athlete starts with 1 special protein bar (More energy = more moves and points)" << endl;
+        characterClassroom = "Dugan Wellness Center";
             break;
         case 'g': cout << "The Geek has an umbrella to avoid getting wet (Easier to travel)" << endl;
+        characterClassroom = "Science Center";
             break;
         default: cout << "Invalid input" << endl;
     }
@@ -31,21 +31,55 @@ int main()
     // Start of the Game
     cout << "When ready, press the enter key to proceed with the game." << endl;
     cin.get();
-    cin.get(ch);
+    cin.get(continueGame);
+
+    // Intro text
+    cout << "You just finished playing a game of chess in the library and feel quite hungry, your next class begins in 45 minutes." << endl;
+    cout << "Make it to the dining hall to sooth your hunger before you make it to your next class in the " << characterClassroom << "." << endl << endl;
+
+    // Game mechanics
+    cout << "In this game you can either move forward, backward, left or right." << endl;
+    cout << "You can also type in actions such as examine, use, and store followed by the desired item." << endl << endl;
+
 
     // Game loop continues while the endgame flag is false
-    while (!endgame)
-    {
-        cout << "       " << setw(6) << " Forward (F)" << endl;
-        cout << setw(6) << "Left (L) " << "          " << " Right (R)"<< endl;
-        cout << "       " << setw(6) << " Backward (B)" << endl << endl;
+    while (characterStamina > NO_STAMINA) {
+        string location = "Library";
 
+        cout << "       " << setw(6) << " Forward (f)" << endl;
+        cout << setw(6) << "Left (l) " << "          " << " Right (r)"<< endl;
+        cout << "       " << setw(6) << " Backward (b)" << endl << endl;
+        cout << "  " << setw(6) << characterStamina << " Stamina Left" << endl << endl;
         cin >> playerDecision;
         characterStamina -= 1;
 
-        cout << characterStamina << endl;
+        // Warning that the player is running out of turns
+        if (characterStamina < 15)
+            cout << "You're feeling tired..." << endl;
+        
+        // Decision tree below based on player movement choices, and characterStamina.
+        // definitely need to change how the movement system will work, need to use a method that doesnt use character
+        // stamina to limit interactions
+        
+        // Library
+        if (location == "Library") {
+            if (playerDecision == "f" && characterStamina == 24)
+                cout << "You walk to the sliding doors, it seems to be raining outside." << endl;
+            else if (playerDecision == "f" && characterStamina == 23) {
+                cout << "Walking through the sliding doors, you enter the Breezeway" << endl;
+                location = "Breezeway";
+            }
+        }
 
-        // Decision tree based on player movement choices, and characterStamina.
+        // Breezeway
+        if (location == "Breezeway") {
+            if (playerDecision == "f" && characterStamina == 23)
+                cout << "You are now in the Breezeway" << endl;
+            else if (playerDecision == "f" && characterStamina == 22)
+                cout << "Walking straight, you're on a path in between starbucks and a statue." << endl;
+        }
+
+        /* playerDecision is stored as a char here still
         if (playerDecision == 'F' && characterStamina == 24)
             cout << "You walk to the sliding doors, it seems to be raining outside." << endl;
         else if (playerDecision == 'B' && characterStamina == 24)
@@ -56,11 +90,15 @@ int main()
             cout << "Theres the help desk! Looks like someone is waiting for you to ask a question." << endl;
         else
             cout << "Invalid input" << endl;
+        */
 
-        if (characterStamina < 15)
-            cout << "You're feeling tired..." << endl;
 
     }
-
+    
+    // End text to show user stats, etc.
+    if (characterStamina == 0) {
+        cout << "Game Over. You ran out of time and missed your class." << endl;
+        cout << "You finished with a total of " << hungerBar << " points!";
+    }
 
 }
