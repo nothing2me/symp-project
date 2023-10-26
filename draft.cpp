4caac524b9faf14fd7ddcdc210888abf34552251
Noah Wilborn <93677972+nothing2me@users.noreply.github.com>
2023-10-26 18:56:43 -0500

    Update draft.cpp

--- draft.cpp
+++ draft.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -6,10 +7,12 @@ int main()
 {
     char continueGame;
     const int NO_STAMINA = 0;
-    int characterClass, hungerBar = 0, characterStamina = 25;
-    bool justStarted = true;
-    string characterClassroom, playerDecision, playerLocationBank;
-    string playerLocation, playerLocationMessage;
+    int characterClass, hungerBar = 100, characterStamina = 25;
+    string characterClassroom, 
+           playerDecision,
+           playerLocationBank,
+           playerLocation,
+           playerLocationMessage;
 
 
     // Splash Screen
@@ -32,50 +35,49 @@ int main()
         cout << "Geek Class    (2) - Starts with umbrella" << endl;
 
         if (cin >> characterClass && characterClass < 3 && characterClass > 0) {
-            playerLocation = "Library";
                 break;
-        } else { // Clears the cin if characterClass is something other than an int, such as a char or string
+        } else { // Clears the input if characterClass is something other than an int, such as a char or string
             cin.clear();
             cin.ignore();
             cout << "Invalid Input" << endl;
             continue;
         }
     } while(true);
-
+    
     // Cases for each class, needs to include items into player inventory before break also
     switch (characterClass)
     {
         case 1: cout << "The Athlete starts with 1 special protein bar (More energy = more moves and points)" << endl;
         characterClassroom = "Dugan Wellness Center";
+        playerLocation = "Library"; // Set starting location
             break;
         case 2: cout << "The Geek has an umbrella to avoid getting wet (Easier to travel)" << endl;
         characterClassroom = "Science Center";
+        playerLocation = "Library"; // Set starting location
             break;
-        default: cout << "Invalid input" << endl;
+        default: cout << "Invalid input" << endl <<endl;
     }
 
     // Start of the Game
     cout << "When ready, press the enter key to proceed with the game." << endl;
     cin.get();
     cin.get(continueGame);
+    
     // Intro text
     cout << "You just finished playing a game of chess in the library and feel quite hungry, your next class begins in 45 minutes." << endl;
     cout << "Make it to the dining hall to sooth your hunger before you make it to your next class in the " << characterClassroom << "." << endl << endl;
     cout << "press the enter key to proceed with the game." << endl;
     cin.get(continueGame);
+    
     // Explain game mechanics
     cout << "Move either forward, backward, left or right." << endl;
     cout << "You can also type in actions such as examine, use, and store followed by the item name." << endl << endl;
-/*
-    // Set Starting conditions
-    if (justStarted){
-        playerLocation = "Library";
-    }
-*/
+    
     // Game loop continues while the endgame flag is false
     while (characterStamina > NO_STAMINA) {
         characterStamina -= 1;
-        // Decision tree below based on player movement choices, and characterStamina.
+        
+        // Decision tree below is based on player movement choices, and characterStamina.
         // definitely need to change how the movement system will work, need to use a method that doesn't use character
         // stamina to limit interactions
         // Library * this is restructured so that playerLocationMessage will print correctly
@@ -87,11 +89,11 @@ int main()
                 playerLocationMessage = "Walking through the sliding doors, you enter the Breezeway";
             }
             else if (playerDecision == "f" && characterStamina == 21 ){
-                justStarted = false;
                 playerLocation = "Breezeway";
 
             }
         }
+        
         // Breezeway
         if (playerLocation == "Breezeway") {
             if (playerDecision == "f" && characterStamina == 21)
@@ -105,12 +107,14 @@ int main()
             else
                 playerLocationMessage = "error";
         }
+        
         // Hector plaza
         if (playerLocation == "Hector") {
             if (playerDecision == "f" && characterStamina == 18) {
                 playerLocationMessage = "You can pickup the item, or chose to move";
             }
         }
+        
         // UI
         cout << "|| " << playerLocationMessage << endl;
         cout << "|]===============================[|" << endl;
@@ -118,14 +122,16 @@ int main()
         cout << "|| Left (l)            Right (r) ||"<< endl;
         cout << "||         Backward (b)          ||" << endl;
         cout << "||                               ||" << endl;
-        cout << "||      " << characterStamina <<"  Stamina Left         ||" << endl;
+        cout << "||         " << characterStamina <<" Stamina" << setw(14) << "||" << endl;
+        cout << "||         "<< hungerBar << " Hunger" << setw(14) << "||" << endl;
         cout << "|]===============================[|" << endl;
         cin >> playerDecision;
 
-        // Warning that the player is running out of turns
-        if (characterStamina < 15)
-            cout << "You're feeling tired..." << endl;
+        // Warning that the player is running out of turns and hunger
+        if (hungerBar <= 40)
+            cout << "You're feeling tired, Find food to refill your stamina." << endl;
     }
+    
     // End text to show user stats, etc.
     if (characterStamina == 0) {
         cout << "Game Over. You ran out of time and missed your class." << endl;
